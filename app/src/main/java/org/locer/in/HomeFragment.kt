9a999fe3209d31c2d96aef53097d93946b31fe43
Rohit Sharma <65807152+Rohit-2602@users.noreply.github.com>
2021-07-03 12:24:13 +0530

    UI Updates

--- app/src/main/java/org/locer/in/HomeFragment.kt
+++ app/src/main/java/org/locer/in/HomeFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.toolbar_home.*
 import org.locer.`in`.databinding.FragmentHomeBinding
-import org.locer.`in`.recyclerView.Datasource
+import org.locer.`in`.databinding.ToolbarHomeBinding
 import org.locer.`in`.recyclerView.ItemAdapter
+import org.locer.`in`.utils.PopularFood
 
 class HomeFragment : Fragment() {
 
@@ -21,19 +25,31 @@ class HomeFragment : Fragment() {
     ): View? {
         homeBoundLayout =
             DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
-        val toolbar = homeBoundLayout.toolbarHomeFrag
-        val activity = activity as MainActivity
-        activity.setSupportActionBar(toolbar)
-        activity.supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(false)
-            setTitle("Home Fragment")
-        }
+//        val toolbar = homeBoundLayout.toolbarHomeFrag as Toolbar
+//        val activity = activity as MainActivity
+//        activity.setSupportActionBar(toolbar)
+//        activity.supportActionBar?.apply {
+//            setDisplayHomeAsUpEnabled(false)
+//            setTitle("Home Fragment")
+//        }
         return homeBoundLayout.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        homeBoundLayout.recyclerView.adapter = ItemAdapter(this, Datasource().loadAffirmations())
+
+//        val toolbarHomeBinding = ToolbarHomeBinding.inflate(LayoutInflater.from(requireContext()))
+//        toolbarHomeBinding.searchView.clearFocus()
+
+        searchView.clearFocus()
+
+        val item = PopularFood("Chowmin", "Enjoy the best chowmin in town with superfast", R.drawable.demo_image)
+        val list = arrayListOf(item, item, item, item, item)
+        homeBoundLayout.recyclerView.apply {
+            adapter = ItemAdapter(list)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
+//        homeBoundLayout.recyclerView.adapter = ItemAdapter(this, list)
     }
 
 }
--- app/src/main/java/org/locer/in/recyclerView/ItemAdapter.kt
+++ app/src/main/java/org/locer/in/recyclerView/ItemAdapter.kt
@@ -4,32 +4,33 @@ package org.locer.`in`.recyclerView
 This class is the adapter i.e. brain of how recycler view will look
  */
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import org.locer.`in`.HomeFragment
-import org.locer.`in`.R
+import org.locer.`in`.databinding.ItemHomeRvBinding
+import org.locer.`in`.utils.PopularFood
 
-class ItemAdapter(private val context: HomeFragment, private val dataset: List<Affirmation>) :
+class ItemAdapter(private val popularFoodList: List<PopularFood>) :
     RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     //This class is holding all the views that we want to display in a container of recycler view
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.textView)
+    class ItemViewHolder(private val binding: ItemHomeRvBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: PopularFood) {
+            binding.popularItemName.text = item.name
+            binding.popularItemDescription.text = item.description
+            binding.popularItemImage.setImageResource(item.image)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        return ItemViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.list_item, parent, false)
-        )
+        val binding = ItemHomeRvBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        return dataset.size
+        return popularFoodList.size
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        holder.textView.text = context.resources.getString(dataset[position].stringResourceId)
+        val currentItem = popularFoodList[position]
+        holder.bind(currentItem)
     }
 }
